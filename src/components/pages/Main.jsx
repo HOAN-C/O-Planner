f3c3638d4ee547e85a2b1fbac1ef246a3930a219
HOAN_C <hoan.c9907@gmail.com>
2023-12-06 16:30:07 +0900

    add Loading Component & style

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,4 +1,6 @@
-//기초교양(교양 필수), 기초교양(창의와 융합), 인간과예술(융합), 사회와역사(융합), 자연과학(융합), 세계와언어(융합), 자유교양, 계열교양, 전공필수, 전공선택, 졸업학점
+import { useState } from "react";
+import Loading from "./Loading";
+
 const section = [
   "기초교양(교양 필수)",
   "기초교양(창의와 융합)",
@@ -14,6 +16,10 @@ const section = [
 ];
 
 export default function Main() {
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // setTimeout(setIsLoading(false), 21000);
+
   return (
     <div>
       <ul>
