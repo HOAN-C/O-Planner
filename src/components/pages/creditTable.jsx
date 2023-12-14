b1eb8dc4fb251007461402e3ec8270239a270691
HOAN_C <hoan.c9907@gmail.com>
2023-12-15 00:54:21 +0900

    명언 데이터 수정, 학점 테이블 수정

--- src/components/pages/creditTable.jsx
+++ src/components/pages/creditTable.jsx
@@ -1,30 +1,18 @@
 import React from "react";
 import styles from "./CreditTable.module.css";
 
-const section = [
-  "기초교양(교양 필수)",
-  "기초교양(창의와 융합)",
-  "인간과예술(융합)",
-  "사회와역사(융합)",
-  "자연과학(융합)",
-  "세계와언어(융합)",
-  "자유교양",
-  "계열교양",
-  "전공필수",
-  "전공선택",
-  "졸업학점",
-];
+const section = ["구분", "전공필수", "전공선택", "졸업학점"];
 
 const creditData = [
-  [14, 4, 0, 2, 2, 2, 0, 9, 30, 36, 120],
-  [15, 4, 0, 2, 2, 2, 0, 9, 30, 27, 91],
-  [1, 0, 0, 0, 2, 0, 0, 0, 3, 9, 22],
+  ["이수 기준", 30, 36, 120],
+  ["이수 학점", 30, 27, 91],
+  ["이수 중 학점", 3, 9, 22],
 ];
 
 export default function CreditTable() {
   return (
     <table className={styles.tableContainer}>
-      {/* 구분 출력 */}
+      {/*구분 출력*/}
       <thead>
         <tr>
           {section.map((item, index) => (
